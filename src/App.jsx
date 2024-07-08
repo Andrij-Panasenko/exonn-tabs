cd5c63bcb2d9ebc51f4cb232a2d2d9124937e361
Andrii Panasenko <130774640+Andrij-Panasenko@users.noreply.github.com>
2024-07-08 17:51:10 +0300

    add render tabs + icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,244 @@
+import './main.css';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import { useState } from 'react';
+import { tabsData } from './mockData/tabsData';
+import CustomTabPanel from './components/CustomTabPanel';
+import sprite from './assets/sprite.svg';
+import { SvgIcon } from '@mui/material';
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 export default function App() {
-  return <></>;
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setTabIndex(newValue);
+  };
+
+  return (
+    <div className="container">
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs value={tabIndex} onChange={handleChange} aria-label="tablist">
+            <Tab
+              style={{ height: '9px' }}
+              icon={
+                <SvgIcon
+                  style={{ width: '16px', height: '16px', marginRight: 10 }}
+                >
+                  <use href={sprite + '#dashboard'}></use>
+                </SvgIcon>
+              }
+              iconPosition="start"
+              label="Dashboard"
+              {...a11yProps(0)}
+            />
+            <Tab
+              icon={
+                <SvgIcon
+                  style={{ width: '16px', height: '16px', marginRight: 10 }}
+                >
+                  <use href={sprite + '#banking'}></use>
+                </SvgIcon>
+              }
+              iconPosition="start"
+              label="Banking"
+              {...a11yProps(1)}
+            />
+            <Tab
+              icon={
+                <SvgIcon
+                  style={{ width: '16px', height: '16px', marginRight: 10 }}
+                >
+                  <use href={sprite + '#telephone'}></use>
+                </SvgIcon>
+              }
+              iconPosition="start"
+              label="Telefonie"
+              {...a11yProps(2)}
+            />
+            <Tab
+              icon={
+                <SvgIcon
+                  style={{ width: '16px', height: '16px', marginRight: 10 }}
+                >
+                  <use href={sprite + '#accounting'}></use>
+                </SvgIcon>
+              }
+              iconPosition="start"
+              label="Accounting"
+              {...a11yProps(3)}
+            />
+            <Tab
+              icon={
+                <SvgIcon
+                  style={{ width: '16px', height: '16px', marginRight: 10 }}
+                >
+                  <use href={sprite + '#shop'}></use>
+                </SvgIcon>
+              }
+              iconPosition="start"
+              label="Verkauf"
+              {...a11yProps(4)}
+            />
+            <Tab
+              icon={
+                <SvgIcon
+                  style={{ width: '16px', height: '16px', marginRight: 10 }}
+                >
+                  <use href={sprite + '#statistic'}></use>
+                </SvgIcon>
+              }
+              iconPosition="start"
+              label="Statistik"
+              {...a11yProps(5)}
+            />
+            <Tab
+              icon={
+                <SvgIcon
+                  style={{ width: '16px', height: '16px', marginRight: 10 }}
+                >
+                  <use href={sprite + '#mail'}></use>
+                </SvgIcon>
+              }
+              iconPosition="start"
+              label="Post Office"
+              {...a11yProps(6)}
+            />
+            <Tab
+              icon={
+                <SvgIcon
+                  style={{ width: '16px', height: '16px', marginRight: 10 }}
+                >
+                  <use href={sprite + '#admin'}></use>
+                </SvgIcon>
+              }
+              iconPosition="start"
+              label="Administration"
+              {...a11yProps(7)}
+            />
+            <Tab
+              icon={
+                <SvgIcon
+                  style={{ width: '16px', height: '16px', marginRight: 10 }}
+                >
+                  <use href={sprite + '#help'}></use>
+                </SvgIcon>
+              }
+              iconPosition="start"
+              label="Help"
+              {...a11yProps(8)}
+            />
+            <Tab
+              icon={
+                <SvgIcon
+                  style={{ width: '16px', height: '16px', marginRight: 10 }}
+                >
+                  <use href={sprite + '#cube'}></use>
+                </SvgIcon>
+              }
+              iconPosition="start"
+              label="Warenbestand"
+              {...a11yProps(9)}
+            />
+            <Tab
+              icon={
+                <SvgIcon
+                  style={{ width: '16px', height: '16px', marginRight: 10 }}
+                >
+                  <use href={sprite + '#list'}></use>
+                </SvgIcon>
+              }
+              iconPosition="start"
+              label="Auswahllisten"
+              {...a11yProps(10)}
+            />
+            <Tab
+              icon={
+                <SvgIcon
+                  style={{ width: '16px', height: '16px', marginRight: 10 }}
+                >
+                  <use href={sprite + '#shopping-cart'}></use>
+                </SvgIcon>
+              }
+              iconPosition="start"
+              label="Einkauf"
+              {...a11yProps(11)}
+            />
+            <Tab
+              icon={
+                <SvgIcon
+                  style={{ width: '16px', height: '16px', marginRight: 10 }}
+                >
+                  <use href={sprite + '#bills'}></use>
+                </SvgIcon>
+              }
+              iconPosition="start"
+              label="Rechn"
+              {...a11yProps(12)}
+            />
+            <Tab
+              icon={
+                <SvgIcon
+                  style={{ width: '16px', height: '16px', marginRight: 10 }}
+                >
+                  <use href={sprite + '#storage'}></use>
+                </SvgIcon>
+              }
+              iconPosition="start"
+              label="Lagerverwaltung"
+              {...a11yProps(13)}
+            />
+            <Tab
+              icon={
+                <SvgIcon
+                  style={{ width: '16px', height: '16px', marginRight: 10 }}
+                >
+                  <use href={sprite + '#shop'}></use>
+                </SvgIcon>
+              }
+              iconPosition="start"
+              label="Verkauf"
+              {...a11yProps(14)}
+            />
+            <Tab
+              icon={
+                <SvgIcon
+                  style={{ width: '16px', height: '16px', marginRight: 10 }}
+                >
+                  <use href={sprite + '#mail'}></use>
+                </SvgIcon>
+              }
+              iconPosition="start"
+              label="Post Office"
+              {...a11yProps(15)}
+            />
+            <Tab
+              icon={
+                <SvgIcon
+                  style={{ width: '16px', height: '16px', marginRight: 10 }}
+                >
+                  <use href={sprite + '#telephone'}></use>
+                </SvgIcon>
+              }
+              iconPosition="start"
+              label="Telefonie"
+              {...a11yProps(16)}
+            />
+          </Tabs>
+        </Box>
+        {tabsData.map((data) => (
+          <CustomTabPanel key={data.id} value={tabIndex} index={data.id}>
+            {data.label}
+          </CustomTabPanel>
+        ))}
+      </Box>
+    </div>
+  );
 }
---
+++ src/mockData/tabsData.js
@@ -0,0 +1,18 @@
+export const tabsData = [
+  { label: 'Dashboard', id: 0 },
+  { label: 'Banking', id: 1 },
+  { label: 'Telefonie', id: 2 },
+  { label: 'Accounting', id: 3 },
+  { label: 'Verkauf', id: 4 },
+  { label: 'Statistik', id: 5 },
+  { label: 'Post Office', id: 6 },
+  { label: 'Administration', id: 7 },
+  { label: 'Help', id: 8 },
+  { label: 'Warenbestand', id: 9 },
+  { label: 'Auswahllisten', id: 10 },
+  { label: 'Einkauf', id: 11 },
+  { label: 'Lagerverwaltung', id: 12 },
+  { label: 'Verkauf', id: 13 },
+  { label: 'Post Office', id: 14 },
+  { label: 'Telefonie', id: 15 }
+];
